0121a408d1b0491c137620ec03e52ada959b7145
Aravind Roy <aravindroy1709@gmail.com>
2022-11-07 14:10:57 +0530

    Usermodel updated

---
+++ server/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name :{
+        type:String ,
+        required: [true , "Name is required"],
+    },
+    email:{
+        type:String , 
+        required:[true , "Email is required"],
+        unique : true,
+        trim: true,
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ],
+    password:{
+        type:String , 
+        required:[true, "Please add a password"],
+        minLenght: [6 , "Enter a password with more than 6 characters"],
+        maxLenght: [20 , "Please limit your password to less than 20 characters"],
+    },
+    CV :{
+        type:String , 
+        required:[true, "Please add a CV"]
+    },
+    phoneNo:{
+        type:String , 
+        default : "+91"
+    },
+    description:{
+        type:String,
+        default: "Reason not defined",
+        maxLength:[250 , "Please limit your reason to 250 characters"]
+    }
+    }
+},{
+    timestamps : true
+})
+
+const UserModel = mongoose.model("UserModel" , userSchema)
+
+module.exports = UserModel
