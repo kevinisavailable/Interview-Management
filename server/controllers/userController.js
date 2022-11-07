92347735a2799428482b6c17a240d6fb425cf7bd
Aravind Roy <aravindroy1709@gmail.com>
2022-11-07 21:54:26 +0530

    User logout Functionality added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -106,8 +106,19 @@ const loginUser = asyncHandler( async(req,res)=>{
     }
     
 })
-
+const logoutUser = asyncHandler( async(req,res)=>{
+    res.cookie("token", "" , {
+        path:"/",
+        httpOnly: true,
+        expires:new Date(0),
+        sameSite:"none",
+        secure:true
+    })
+    return res.status(200).json({
+        message:"User successfully logged Out"
+    })
+})
 
 module.exports = {
-    registerUser, loginUser
+    registerUser, loginUser ,logoutUser
 }
