11828a7f5d2487536e2f37332281807ea0dd866c
Aravind Roy <aravindroy1709@gmail.com>
2022-11-08 21:29:30 +0530

    Reset Password design completed

--- client/src/pages/auth/Reset.jsx
+++ client/src/pages/auth/Reset.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import Card from '../../components/Card'
+import {Link} from 'react-router-dom'
+
 
 const Reset = () => {
   return (
-    <div>Reset</div>
+    <div className='container justify-content-center align-items-center w-25 mt-5'>
+    <Card>
+    <main class="form-signin">
+  <form>
+    
+    <h1 class="h3 mb-3 fw-normal text-center">Reset Password</h1>
+
+    <div class="form-floating my-2">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required/>
+      <label for="floatingPassword">Password</label>
+    </div>
+    <div class="form-floating my-2">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required/>
+      <label for="floatingPassword">Confirm Password</label>
+    </div>
+    <div className='text-center'>
+    <button class="w-50 btn btn-md btn-primary text-center" type="submit">Reset Password</button>
+    </div>
+    <div className='d-flex mx-2 px-2 justify-content-around'>
+    <Link to='/register' style={{textDecoration:"none"}}>
+    <div className='mt-1 text-center text-danger'>
+        <p> Register</p>
+    </div>
+    </Link>
+    <Link to='/login' style={{textDecoration:"none"}}>
+    <div className='mt-1 text-center'>
+        <p>Login</p>
+    </div>
+    </Link>
+    </div>
+  </form>
+</main>
+    </Card>
+    
+    </div>
   )
 }
 
