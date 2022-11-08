088ea50cb278f91646c1195d1645d371bb58005a
Aravind Roy <aravindroy1709@gmail.com>
2022-11-08 14:19:52 +0530

    Product Model created

---
+++ server/models/productModel.js
@@ -0,0 +1,44 @@
+const mongoose  = require('mongoose')
+
+const productSchema  =new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        require:true,
+        ref:"User"
+    },
+    name:{
+        type:String,
+        required:[true , "Please add a name"],
+        trim:true
+    },
+    sku:{
+        required:true,
+        default:"SKU",
+        trim:true
+    },
+    category:{
+        required:[true,"Please add a category"],
+        type:String,
+    },
+    quantity:{
+        required:[true , "Please add a quantity"],
+        type:String
+    },
+    price:{
+        required:[true , "Please add a price"],
+        type:String,
+        trim:true
+    },
+    description:{
+        required:[true , "Please add a description"],
+        trim:true,
+        type:String
+    }
+},{
+    timestamps:true
+})
+
+
+const Product = mongoose.model("Product" , productSchema)
+
+module.exports = Product
