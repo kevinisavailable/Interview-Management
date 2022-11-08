38a74ca12e09da479bdeda94abbabbcefa3fd4d1
Aravind Roy <aravindroy1709@gmail.com>
2022-11-09 02:04:27 +0530

    Updated Header and Sidebar

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,4 +1,4 @@
-
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -6,28 +6,31 @@ const Header = () => {
   <header id="header" className="header fixed-top d-flex align-items-center">
 
     <div className="d-flex align-items-center justify-content-between">
-      <a href="index.html" className="logo d-flex align-items-center">
+      <Link to='/'>
+      <a className="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt=""/>
-        <span className="d-none d-lg-block">NiceAdmin</span>
+        <span className="d-none d-lg-block">Interview </span>
       </a>
-      <i className="bi bi-list toggle-sidebar-btn"></i>
+      </Link>
+      
+      {/* <i className="bi bi-list toggle-sidebar-btn"></i> */}
     </div>
 
-    <div className="search-bar">
+    {/* <div className="search-bar">
       <form className="search-form d-flex align-items-center" method="POST" action="#">
         <input type="text" name="query" placeholder="Search" title="Enter search keyword"/>
         <button type="submit" title="Search"><i className="bi bi-search"></i></button>
       </form>
-    </div>
+    </div> */}
 
     <nav className="header-nav ms-auto">
       <ul className="d-flex align-items-center">
 
-        <li className="nav-item d-block d-lg-none">
+        {/* <li className="nav-item d-block d-lg-none">
           <a className="nav-link nav-icon search-bar-toggle " href="#">
             <i className="bi bi-search"></i>
           </a>
-        </li>
+        </li> */}
         <li className="nav-item dropdown pe-3">
 
           <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
@@ -47,7 +50,9 @@ const Header = () => {
             <li>
               <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i className="bi bi-person"></i>
+                <Link to='/profile'>
                 <span>My Profile</span>
+                </Link>
               </a>
             </li>
             <li>
@@ -57,7 +62,7 @@ const Header = () => {
             <li>
               <a className="dropdown-item d-flex align-items-center" href="#">
                 <i className="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
+                <span>Log Out</span>
               </a>
             </li>
 
--- client/src/components/Layout/Sidebar.jsx
+++ client/src/components/Layout/Sidebar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import {Link} from 'react-router-dom'
 
 const Sidebar = () => {
   return (
@@ -6,17 +7,38 @@ const Sidebar = () => {
       <aside id="sidebar" className="sidebar">
         <ul className="sidebar-nav" id="sidebar-nav">
           <li className="nav-item">
-            <a className="nav-link " href="">
+            <Link to='/dashboard'>
+            <a className='nav-link'>
               <i className="bi bi-grid"></i>
               <span>Dashboard</span>
             </a>
+            </Link>
           </li>
 
           <li className="nav-item">
-            <a className="nav-link " href="#">
-              <i className="bi bi-menu-button-wide"></i>
-              <span>Components</span>
+            <Link to='/add-product'>
+            <a className='nav-link collapsed'>
+            <i class="bi bi-plus-circle"></i>
+              <span>Add Product</span>
             </a>
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to='contact-us'>
+            <a className='nav-link collapsed'>
+            <i class="bi bi-bug-fill"></i>
+              <span>Report Bug</span>
+            </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='view-candidates'>
+            <a className='nav-link collapsed'>
+            <i class="bi bi-people-fill"></i>
+              <span>View Candidates</span>
+            </a>
+            </Link>
           </li>
         </ul>
       </aside>
