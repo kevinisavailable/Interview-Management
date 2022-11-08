41e5330b5736d69bd166f1f357dee0833d279f75
Aravind Roy <aravindroy1709@gmail.com>
2022-11-08 20:47:48 +0530

    Auth pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Homepage from './pages/Homepage';
+import {Forgot , Login ,Register ,Reset} from './pages/auth/auth'
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Homepage />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/forgotpassword' element={<Forgot />}/>
+      <Route path='/resetpassword' element={<Reset />}/>
     </Routes>
     </BrowserRouter>
   );
