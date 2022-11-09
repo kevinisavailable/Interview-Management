caef8edf3a633eda38cabc889b24f62227542219
Aravind Roy <aravindroy1709@gmail.com>
2022-11-09 17:20:29 +0530

    updated Login status

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,24 @@ import {Forgot , Login ,Register ,Reset} from './pages/auth/auth'
 import Layout from './components/Layout/Layout';
 import axios from 'axios'
 import { Toaster } from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getLoginStatus } from './services/authService';
+import { SET_LOGIN } from './redux/features/auth/authSlice';
 axios.defaults.withCredentials = true
+
 function App() {
+const dispatch = useDispatch()
+useEffect(() => {
+  async function loginStatus(){
+    const status = await getLoginStatus()
+    dispatch(SET_LOGIN(status))
+  }
+  loginStatus()
+}, [dispatch])
+  
+
+
   return (
     <BrowserRouter>
     <Toaster
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -81,3 +81,16 @@ export const ResetPassword =async(userData , resetToken)=>{
         
     }
 }
+
+
+export const getLoginStatus =async()=>{
+    try{
+        const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`)
+        return(response.data)
+    }   
+    catch(error){
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        toast.error(message)
+        
+    }
+}
