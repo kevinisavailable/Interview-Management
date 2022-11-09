42c8dc0982a2b97b7fae394ce5f497af56f4c837
Aravind Roy <aravindroy1709@gmail.com>
2022-11-10 00:16:30 +0530

    Product Slice Async Thunk created

---
+++ client/src/redux/features/product/productService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+const API_URL = `${BACKEND_URL}/api/products`
+
+
+export const createProduct = async(formData)=>{
+   const response = await axios.post(`${API_URL}/create` , formData)
+   return response.data
+}
--- client/src/redux/features/product/productSlice.js
+++ client/src/redux/features/product/productSlice.js
@@ -1,5 +1,6 @@
 import { createSlice , createAsyncThunk } from '@reduxjs/toolkit'
-
+import { createProduct } from './productService';
+import {toast} from 'react-hot-toast'
 const initialState = {
     product: null,
     products :[],
@@ -8,6 +9,21 @@ const initialState = {
     isLoading :false,
     message:""
 }
+
+const createNewProduct = createAsyncThunk(
+    "products/create", 
+    async(formData , thunkAPI)=>{
+        try{
+            return await createProduct(formData)
+        }
+        catch(error){
+            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString() 
+            console.log(message)
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
 
 const productSlice = createSlice({
   name: "product",
@@ -18,7 +34,23 @@ const productSlice = createSlice({
     }
   },
   extraReducers:(builder)=>{
-
+    builder
+    .addCase(createNewProduct.pending , (state)=>{
+        state.isLoading = true
+    })
+    .addCase(createNewProduct.fulfilled , (state , action)=>{
+        state.isLoading = false
+        state.isSuccess = true
+        console.log(action.payload)
+        state.products.push(action.payload)
+        toast.success("Product added successfully")
+    })
+    .addCase(createNewProduct.rejected , (state , action)=>{
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        toast.error(action.payload)
+    })
   }
 });
 
