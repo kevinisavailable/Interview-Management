cb1a876437e26f6604732d907a0f94d21248673d
Aravind Roy <aravindroy1709@gmail.com>
2022-11-10 15:09:17 +0530

    User Data fetched

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import Layout from '../../components/Layout/Layout'
 import useRedirectLogout from '../../customHook/useRedirectLogout'
+import { SAVE_USER, SET_NAME } from '../../redux/features/auth/authSlice'
+import { getUserProfile } from '../../services/authService'
 
 const Profile = () => {
-    useRedirectLogout('/login')
+    const[profile , setProfile] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        setIsLoading(true)
+        async function getUser(){
+            const data = await getUserProfile()
+            console.log(data)
+        
+        setProfile(data)
+        setIsLoading(false)
+        await dispatch(SAVE_USER(data))
+        await dispatch(SET_NAME(data.name))
+        }
+        getUser()
+    }, [dispatch])
+    
   return (
+    <Layout>
     <div>Profile</div>
+    </Layout>
   )
 }
 
